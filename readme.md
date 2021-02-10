c0573c01a559eadf4089db341db81e4d0deca26d
Wendell Boldrini <Wendell_boldrini@hotmail.com>
2021-02-09 23:58:34 -0300

    Segundo Commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Olá Estou aprendendo java  
+
+## Meu nome é Wendell Boldrini
+
+## Meu e-mail é Wendell_boldrini@hotmail.com  
--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,6 @@ package fundamentos;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
+		System.out.println("Primeiro Programa!!!");
 	}
 }
